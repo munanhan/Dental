35ae43dc212299e217fd2d6dff4e99c1bb23cdf5
Kuang917 <775803639@qq.com>
2019-04-25 09:07:41 +0800

    修改

--- client/src/views/patient/AddPatient.vue
+++ client/src/views/patient/AddPatient.vue
@@ -6,22 +6,23 @@
     class="custom-dialog"
     v-dialog-drag
     width="750px"
-    top="3vh"
   >
     <el-form
       ref="changePwdForm"
       label-width="80px"
-      :rules="rules"
+      :rules="form.rules"
+      :model="form"
     >
       <div style="display:flex">
         <el-form-item
           label="病历号"
           style="width:300px"
           size="small"
-          prop="name"
+          prop="anamnesis"
         >
-          <el-input v-model="ruleForm.name"></el-input>
+          <el-input v-model="form.anamnesis"></el-input>
         </el-form-item>
+
         <i
           class="fa fa-cog"
           style="margin-top:10px;
@@ -29,6 +30,7 @@
                  font-size:20px;
                  cursor:pointer"
         ></i>
+
         <div
           class="block"
           style="margin-top:10px;
@@ -36,7 +38,7 @@
                  margin-right:40px"
         >
           <span class="demonstration"></span>
-          <el-rate v-model="value1"></el-rate>
+          <el-rate v-model="form.value1"></el-rate>
         </div>
         <div class="block">
           <el-button
@@ -66,22 +68,22 @@
           size="small"
           prop="name"
         >
-          <el-input></el-input>
+          <el-input v-model="form.name"></el-input>
         </el-form-item>
 
         <div style="margin-top:10px;margin-left:50px">
           性&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           别&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <el-radio
-            v-model="sex"
+            v-model="form.sex"
             label="1"
           >男</el-radio>
           <el-radio
-            v-model="sex"
+            v-model="form.sex"
             label="2"
           >女</el-radio>
           <el-radio
-            v-model="sex"
+            v-model="form.sex"
             label="3"
           >未填</el-radio>
         </div>
@@ -90,26 +92,34 @@
         <div style="display:flex">
           <el-form-item
             label="电话1"
-            prop="tel_one"
-            style="width:215px"
+            prop="phone_one"
+            style="width:210px"
           >
             <el-input
-              v-model="tel_one"
               placeholder="请输入内容"
+              v-model="form.phone_one"
             ></el-input>
           </el-form-item>
           <el-select
-            v-model="value"
-            placeholder="选择"
-            style="width:85px"
+            v-model="form.tel_one"
+            style="width:90px"
           >
             <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            >
-            </el-option>
+              label="   "
+              value="zhuhai"
+            ></el-option>
+            <el-option
+              label="爸爸"
+              value="shanghai"
+            ></el-option>
+            <el-option
+              label="妈妈"
+              value="beijing"
+            ></el-option>
+            <el-option
+              label="妈妈妈"
+              value="beijing"
+            ></el-option>
           </el-select>
           <i
             class="fa fa-cog"
@@ -122,21 +132,21 @@
 
           <el-form-item
             label="电话2"
-            prop="tel_one"
+            prop="phone_two"
             style="width:210px"
           >
             <el-input
-              v-model="tel_one"
               placeholder="请输入内容"
+              v-model="form.phone_two"
             ></el-input>
           </el-form-item>
           <el-select
-            v-model="value"
+            v-model="form.tel_two"
             placeholder="选择"
-            style="width:80px"
+            style="width:90px"
           >
             <el-option
-              v-for="item in options"
+              v-for="item in form.tel_two"
               :key="item.value"
               :label="item.label"
               :value="item.value"
@@ -151,30 +161,50 @@
             prop="birthday"
             style="width:300px"
           >
-            <el-input autocomplete="off"></el-input>
+            <el-input
+              autocomplete="off"
+              v-model="form.birthday"
+            ></el-input>
           </el-form-item>
           <el-form-item
             style="margin-left:30px;width:300px"
-            label="年龄"
+            label="年 龄"
+            prop="age"
           >
-            <el-input></el-input>
+            <el-input v-model="form.age"></el-input>
           </el-form-item>
         </div>
 
         <div style="display:flex">
-          <el-form-item label="患者来源">
+          <el-form-item
+            label="患者来源"
+            prop="patients_source"
+          >
             <el-select
-              v-model="value"
+              v-model="form.patients_source"
+              placeholder="请选择活动区域"
               style="width:220px"
-              placeholder="请选择"
             >
               <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              >
-              </el-option>
+                label="   "
+                value="guiling"
+              ></el-option>
+              <el-option
+                label="网络咨询"
+                value="shanghai"
+              ></el-option>
+              <el-option
+                label="朋友介绍"
+                value="beijing"
+              ></el-option>
+              <el-option
+                label="家住附近"
+                value="shenzhen"
+              ></el-option>
+              <el-option
+                label="诊所网站"
+                value="guangzhou"
+              ></el-option>
             </el-select>
           </el-form-item>
           <i
@@ -190,24 +220,27 @@
             类&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             别&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <el-radio
-              v-model="category"
+              v-model="form.category"
               label="1"
             >初诊</el-radio>
             <el-radio
-              v-model="category"
+              v-model="form.category"
               label="2"
             >复诊</el-radio>
           </div>
         </div>
         <div style="display:flex">
-          <el-form-item label="就诊日期">
+          <el-form-item
+            label="就诊日期"
+            prop="clinic_date"
+          >
             <el-select
-              v-model="value"
+              v-model="form.clinic_date"
               style="width:220px"
               placeholder="请选择"
             >
               <el-option
-                v-for="item in options"
+                v-for="item in form.options"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
@@ -220,21 +253,23 @@
             style="margin-left:50px;width:300px"
             label="主治医生"
             prop="attendingphysician"
-            v-model="ruleForm.attendingphysician"
           >
-            <el-input></el-input>
+            <el-input v-model="form.attendingphysician"></el-input>
           </el-form-item>
         </div>
 
         <div style="display:flex">
-          <el-form-item label="患者分类">
+          <el-form-item
+            label="患者分类"
+            prop="classification_patients"
+          >
             <el-select
-              v-model="value"
+              v-model="form.classification_patients"
               style="width:220px"
               placeholder="请选择"
             >
               <el-option
-                v-for="item in options"
+                v-for="item in form.options"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
@@ -252,21 +287,25 @@
           ></i>
           <el-form-item
             style="margin-left:20px;width:300px"
+            prop="patient_note"
             label="患者备注"
           >
-            <el-input></el-input>
+            <el-input v-model="form.patient_note"></el-input>
           </el-form-item>
 
         </div>
         <div style="display:flex">
-          <el-form-item label="会员等级">
+          <el-form-item
+            label="会员等级"
+            prop="membership_grade"
+          >
             <el-select
-              v-model="value"
+              v-model="form.membership_grade"
               style="width:220px"
               placeholder="请选择"
             >
               <el-option
-                v-for="item in options"
+                v-for="item in form.options"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
@@ -286,8 +325,9 @@
           <el-form-item
             style="margin-left:20px;width:300px"
             label="会员卡号"
+            prop="mem_card_number"
           >
-            <el-input></el-input>
+            <el-input v-model="form.mem_card_number"></el-input>
           </el-form-item>
         </div>
         <div>
@@ -305,12 +345,12 @@
               </el-option>
             </el-select> -->
             <el-select
-              v-model="value"
+              v-model="form.value"
               placeholder="请选择"
               style="width:570px"
             >
               <el-option
-                v-for="item in options"
+                v-for="item in form.options"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
@@ -354,20 +394,33 @@
           </el-form-item>
         </div>
         <div style="display:flex">
-          <el-form-item label="社保号">
-            <el-input style="width:220px"></el-input>
+          <el-form-item
+            label="社保号"
+            prop="social_security"
+          >
+            <el-input
+              style="width:220px"
+              v-model="form.social_security"
+            ></el-input>
 
           </el-form-item>
           <el-form-item
             style="margin-left:60px;width:300px"
             label="电子邮箱"
+            porp="email"
           >
-            <el-input></el-input>
+            <el-input v-model="form.email"></el-input>
           </el-form-item>
         </div>
         <div style="display:flex">
-          <el-form-item label="患者印象">
-            <el-input style="width:220px"></el-input>
+          <el-form-item
+            label="患者印象"
+            prop="patients_impression"
+          >
+            <el-input
+              style="width:220px"
+              v-model="form.patients_impression"
+            ></el-input>
           </el-form-item>
           <i
             class="fa fa-cog"
@@ -380,19 +433,23 @@
           <el-form-item
             style="margin-left:30px;width:300px"
             label="电网咨询"
+            prop="grid_consulting"
           >
-            <el-input></el-input>
+            <el-input v-model="form.grid_consulting"></el-input>
           </el-form-item>
         </div>
         <div style="display:flex">
-          <el-form-item label="民族">
+          <el-form-item
+            label="民族"
+            porp="national"
+          >
             <el-select
               style="width:220px"
-              v-model="value"
+              v-model="form.national"
               placeholder="请选择"
             >
               <el-option
-                v-for="item in options"
+                v-for="item in form.options"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
@@ -411,30 +468,47 @@
           <el-form-item
             label="吸烟"
             style="margin-left:30px"
+            prop="smoking"
           >
             <div style="display:flex">
-              <el-input style="width:100px"></el-input>
+              <el-input
+                style="width:100px"
+                v-model="form.smoking"
+              ></el-input>
               <div style="margin-left:10px">次/天</div>
             </div>
           </el-form-item>
         </div>
         <div style="display:flex">
-          <el-form-item label="刷牙">
+          <el-form-item
+            label="刷牙"
+            prop="brush_teeth"
+          >
             <div style="display:flex">
-              <el-input style="width:60px"></el-input>
+              <el-input
+                style="width:60px"
+                v-model="form.brush_teeth_Timeday"
+              ></el-input>
               <div style="margin-left:10px">次/天</div>
             </div>
           </el-form-item>
 
           <div style="display:flex">
-            <el-input style="width:60px;margin-left:10px"></el-input>
-            <div style="margin-left:10px;margin-top:13px">分钟/次</div>
+            <el-input
+              v-model="form.brush_teeth_minutestime"
+              style="width:60px;
+                             margin-left:10px"
+            ></el-input>
+            <div style="margin-left:10px;
+                        margin-top:13px">分钟/次</div>
           </div>
           <el-form-item
             label="洁牙习惯"
-            style="margin-left:15px"
+            prop="teeth_cleaning_habits"
+            style="margin-left:46px;
+                   width:300px"
           >
-            <el-input></el-input>
+            <el-input v-model="form.teeth_cleaning_habits"></el-input>
           </el-form-item>
           <i
             class="fa fa-cog"
@@ -470,56 +544,71 @@
 <script>
 import AddDialogForm from "../base/AddDialogForm";
 export default {
-  name: "ChangePassword",
+  name: "AddPatient",
 
   mixins: [AddDialogForm],
 
   data() {
     return {
-      options: [
-        {
-          value: "选项1",
-          label: "爸爸"
-        },
-        {
-          value: "选项2",
-          label: "双皮奶"
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎"
-        },
-        {
-          value: "选项4",
-          label: "龙须面"
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭"
-        }
-      ],
-      tel_one: "",
-      value: "",
-      ruleForm: {
-        name: "",
-        attendingphysician: ""
-      },
-      value1: null,
-      sex: "1",
-      category: "1",
+      form: {
+        options: [
+          {
+            value: "选项1",
+            label: "爸爸"
+          },
+          {
+            value: "选项2",
+            label: "双皮奶"
+          },
+          {
+            value: "选项3",
+            label: "蚵仔煎"
+          },
+          {
+            value: "选项4",
+            label: "龙须面"
+          },
+          {
+            value: "选项5",
+            label: "北京烤鸭"
+          }
+        ],
+        patients_source: "guiling",
+        birthday: "",
+        tel_one: "zhuhai",
+        phone_one: "",
+        value: "",
+        value1: null,
+        sex: "1",
+        category: "1",
+        mem_card_number: "",
+        social_security: "",
+        email: "",
+        smoking: "",
+        patients_impression: "",
+        patient_note: "",
+        grid_consulting: "",
+        brush_teeth_Timeday: "",
+        brush_teeth_minutestime: "",
+        teeth_cleaning_habits: "",
 
-      rules: {
-        name: [
-          { required: true, message: "请输入病历号", trigger: "blur" }
-          // { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
-        ],
-        attendingphysician: [
-          { required: true, message: "请输入主治医生", trigger: "blur" }
-        ],
-        name: [
-          { required: true, message: "请输入姓名", trigger: "blur" }
-          // { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
-        ]
+        name: "",
+        attendingphysician: "",
+        anamnesis: "",
+
+        rules: {
+          attendingphysician: [
+            { required: true, message: "请输入主治医生", trigger: "blur" }
+          ],
+          name: [
+            { required: true, message: "请输入姓名", trigger: "blur" },
+            { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+          ],
+          anamnesis: [
+            { required: true, message: "请输入病历号", trigger: "blur" },
+            { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+          ]
+        }
       }
     };
   },
--- client/src/views/patient/ReturnVisitInfo.vue
+++ client/src/views/patient/ReturnVisitInfo.vue
@@ -7,88 +7,56 @@
       <div>
         <el-table-column
           prop="date"
-          label="状态"
-          width="55"
-          align="center"
+          label="录音"
+          width="180"
         >
         </el-table-column>
         <el-table-column
           prop="name"
-          label="单据号"
-          width="80"
-          align="center"
+          label="回访状态"
+          width="180"
         >
         </el-table-column>
         <el-table-column
           prop="address"
-          label="收费时间"
-          align="center"
-          width="160"
+          label="回访人"
         >
         </el-table-column>
         <el-table-column
           prop="date"
-          label="医生"
-          width="60"
-          align="center"
+          label="日期"
+          width="180"
         >
         </el-table-column>
         <el-table-column
           prop="name"
-          label="收款员"
-          width="70"
-          align="center"
+          label="姓名"
+          width="180"
         >
         </el-table-column>
         <el-table-column
           prop="address"
-          label="本次费用"
-          width="90"
-          align="center"
+          label="地址"
         >
         </el-table-column>
         <el-table-column
           prop="date"
-          label="实收金额"
-          width="90"
-          align="center"
+          label="日期"
+          width="180"
         >
         </el-table-column>
         <el-table-column
           prop="name"
-          label="折扣金额"
-          width="90"
-          align="center"
+          label="姓名"
+          width="180"
         >
         </el-table-column>
         <el-table-column
           prop="address"
-          label="欠费"
-          width="120"
-          align="center"
+          label="地址"
         >
         </el-table-column>
-        <el-table-column
-          prop="date"
-          label="账单时间"
-          width="160"
-          align="center"
-        >
-        </el-table-column>
-        <el-table-column
-          prop="name"
-          label="护士"
-          width="120"
-          align="center"
-        >
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="备注"
-          width="150"
-          align="center"
-        >
-        </el-table-column>
+
       </div>
     </el-table>
     <div class="charge-bottom">
