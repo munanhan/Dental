2cf9557a399511b61e8581ca23cf44ed0befff57
seasonTree <leung_chikin@outlook.com>
2019-04-28 14:30:19 +0800

    页面

--- client/src/views/statistics/Index.vue
+++ client/src/views/statistics/Index.vue
@@ -69,7 +69,7 @@ import ChainStoreStatistics from "./ChainStoreStatistics";
 import ChargingReconciliation from "./charging_recon/ChargingReconciliation";
 import ClinicOperation from "./clinic_operation/ClinicOperation";
 import EmployeePerformance from "./employee_perform/EmployeePerformance";
-import ExternalProcessing from "./ExternalProcessing";
+import ExternalProcessing from "./external_process/ExternalProcessing";
 import PassengerFlowAnalysis from "./PassengerFlowAnalysis";
 import WechatOperation from "./WechatOperation";
 
