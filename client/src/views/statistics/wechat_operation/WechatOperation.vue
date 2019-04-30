09fb26619e0c721321b7cb2f1b87df06b8aece38
seasonTree <leung_chikin@outlook.com>
2019-04-30 11:58:28 +0800

    也没那

--- client/src/views/statistics/wechat_operation/WechatOperation.vue
+++ client/src/views/statistics/wechat_operation/WechatOperation.vue
@@ -39,19 +39,125 @@
                 趋势图
             </div>
             <div class="tend-content">
-                213213213123123
+                <div class="mb-10">
+                    <span class="mr-20">
+                        <el-date-picker
+                            v-model="trendSearch.dateRange"
+                            type="daterange"
+                            align="right"
+                            unlink-panels
+                            range-separator="至"
+                            start-placeholder="开始日期"
+                            end-placeholder="结束日期"
+                            :picker-options="pickerOptions"
+                            format="yyyy-MM-dd"
+                            value-format="yyyy-MM-dd"
+                            :clearable="false"
+                        >
+                        </el-date-picker>
+                    </span>
+
+                    <span>
+                        <el-radio-group
+                            v-model="trendSearch.type"
+                            @change="trendChange"
+                        >
+                            <el-radio-button :label="0">新增关注</el-radio-button>
+                            <el-radio-button :label="1">取消关注</el-radio-button>
+                            <el-radio-button :label="2">新增预约</el-radio-button>
+                            <el-radio-button :label="3">新增咨询</el-radio-button>
+                        </el-radio-group>
+                    </span>
+                </div>
+
+                <div v-if="showChart">
+                    <ve-line
+                        :data="lineDate"
+                        :settings="lineSettings"
+                    ></ve-line>
+                </div>
+            </div>
+
+        </el-row>
+
+        <el-row class="detail">
+            <div class="action-bar">
+                <div class="search-item">
+                    <el-date-picker
+                        v-model="detailSearch.dateRange"
+                        type="daterange"
+                        align="right"
+                        unlink-panels
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期"
+                        format="yyyy-MM-dd"
+                        value-format="yyyy-MM-dd"
+                        :clearable="false"
+                    >
+                    </el-date-picker>
+                </div>
+                <div class="action-bar-right">
+                    <el-button
+                        type="primary"
+                        @click="exportExcel"
+                    >导出Excel</el-button>
+                </div>
+            </div>
+            <div>
+                <el-table
+                    border
+                    class="width100 mb-10"
+                    :data="tableData"
+                    :header-cell-style="{backgroundColor:'#e3e3e3',color:'#3a3a3a'}"
+                    :height="320"
+                >
+                    <el-table-column
+                        label="时间"
+                        width="120"
+                        align="center"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="新增关注"
+                        align="center"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                        prop="address"
+                        label="取消关注"
+                        align="center"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                        prop="address"
+                        label="新增微信预约"
+                        align="center"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                        prop="address"
+                        label="新增咨询"
+                        align="center"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                        prop="address"
+                        label="总关注人数"
+                        align="center"
+                    >
+                    </el-table-column>
+                </el-table>
             </div>
         </el-row>
     </div>
 </template>
 
 <script>
-import Base from "../Base";
 export default {
     name: "WechatOperation",
     components: {},
-
-    mixins: [Base],
 
     props: {
         refresh: {
@@ -60,14 +166,128 @@ export default {
         }
     },
     data() {
-        return {};
+        return {
+            trendSearch: {
+                dateRange: [new Date(), new Date()],
+                type: 0
+            },
+
+            detailSearch: {
+                dateRange: [new Date(), new Date()]
+            },
+
+            tableData: [],            
+
+            pickerOptions: {
+                shortcuts: [
+                    {
+                        text: "今天",
+                        onClick(picker) {
+                            picker.$emit("pick", [new Date(), new Date()]);
+                        }
+                    },
+                    {
+                        text: "昨天",
+                        onClick(picker) {
+                            let yesterday = getYesterday();
+
+                            picker.$emit("pick", [yesterday, yesterday]);
+                        }
+                    },
+                    {
+                        text: "本周",
+                        onClick(picker) {
+                            let curWeek = getCurWeek();
+
+                            picker.$emit("pick", [curWeek.start, curWeek.end]);
+                        }
+                    },
+                    {
+                        text: "本月",
+                        onClick(picker) {
+                            let curMonth = getCurMonth();
+
+                            picker.$emit("pick", [
+                                curMonth.start,
+                                curMonth.end
+                            ]);
+                        }
+                    }
+                ]
+            },
+
+            lineDate: {
+                columns: ["date", "value"],
+                rows: [
+                    {
+                        date: "2019-01-10",
+                        value: 1393
+                    },
+                    {
+                        date: "2019-01-11",
+                        value: 3530
+                    },
+                    {
+                        date: "2019-01-12",
+                        value: 2923
+                    },
+                    {
+                        date: "2019-01-13",
+                        value: 1723
+                    },
+                    {
+                        date: "2019-01-14",
+                        value: 3792
+                    },
+                    {
+                        date: "2019-01-15",
+                        value: 4593
+                    }
+                ]
+            },
+
+            lineSettings: {
+                labelMap: {
+                    value: "数量"
+                }
+            },
+
+            //因为首次加载时候图表没有显示， 所以要另外处理
+            showChart: false
+        };
     },
     created() {},
     mounted() {},
-    watch: {},
+    watch: {
+        refresh(newValue, oldValue) {
+            let that = this;
+
+            //因为首次加载时候图表没有显示， 所以要另外处理
+            that.showChart = true;
+
+            that.getTrendData();
+
+            //更新原来的refresh, 防止下次点击时不通知更新
+            that.$emit("update:refresh", false);
+        }
+    },
     computed: {},
     methods: {
-        afterGetData(res) {
+        trendChange(value) {
+            let that = this;
+
+            that.getTrendData();
+        },
+
+        getTrendData() {
+            let that = this;
+        },
+
+        getDetailData() {
+            let that = this;
+        },
+
+        exportExcel() {
             let that = this;
         }
     }
@@ -83,6 +303,7 @@ export default {
     box-sizing: border-box;
     background-color: #efefef;
     padding: 10px;
+    overflow: auto;
 
     .gray-border {
         border: 1px solid #e3e3e3;
@@ -100,7 +321,7 @@ export default {
         line-height: 30px;
         display: flex;
         margin-bottom: 10px;
-        
+
         .bg-white;
 
         .head-detail {
@@ -136,6 +357,8 @@ export default {
     }
 
     .trend {
+        margin-bottom: 10px;
+
         .bg-white;
 
         .trend-title {
@@ -149,9 +372,14 @@ export default {
             }
         }
 
-        .tend-content{
+        .tend-content {
             padding: 10px;
         }
+    }
+
+    .detail {
+        padding: 10px 10px 0 10px;
+        .bg-white;
     }
 }
 </style>
