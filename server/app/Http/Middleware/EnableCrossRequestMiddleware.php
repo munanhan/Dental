a4a8389f5b2836754326014bade8667627dd9980
yuqi2060 <yuqi2060@163.com>
2019-05-05 18:48:10 +0800

    allow_origin

--- server/app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ server/app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -17,11 +17,9 @@ class EnableCrossRequestMiddleware
     {
         $response = $next($request);
         $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        $allow_origin = [
-            'http://localhost:8080',
-            'http://127.0.0.1:8080'
-        ];
+        $allow_origin = config('config.allow_origin');//获取允许跨域的列表
         if (in_array($origin, $allow_origin)) {
+            //设置跨域
             $response->header('Access-Control-Allow-Origin', $origin);
             $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
             $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
