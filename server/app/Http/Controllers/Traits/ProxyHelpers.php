deed5475aad43667a76836f7bacf91c684308f7c
seasonTree <leung_chikin@outlook.com>
2019-05-05 18:09:57 +0800

    a

--- server/app/Http/Controllers/Traits/ProxyHelpers.php
+++ server/app/Http/Controllers/Traits/ProxyHelpers.php
@@ -15,7 +15,7 @@ trait ProxyHelpers
      */
     public function authenticate($guard=null)
     {
-        // try {
+        try {
 
             $client = new Client();
 
@@ -26,31 +26,16 @@ trait ProxyHelpers
                 'password' => request('password'),
                 'provider' => $guard,
             ]);
-/*******************************先屏蔽***********************************************************************/
-            // $response = $client->post($url, ['form_params' => $params]);
+            $response = $client->post($url, ['form_params' => $params]);
 
-        //     if($response->getStatusCode() ===200){
-        //         return message('Login successful','200', json_decode($response->getBody()->getContents(), true));
-        //     }
+            if($response->getStatusCode() ===200){
+                return message('Login successful','200', json_decode($response->getBody()->getContents(), true));
+            }
 
-        // } catch (RequestException $exception) {
-        //     return message('Incorrect user name or password',401);
+        } catch (RequestException $exception) {
+            return message('Incorrect user name or password',401);
 
-        // }
-/**********************************************************************************************************/
-        $ch = curl_init();//初始化
-        curl_setopt($ch, CURLOPT_URL, $url);//抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-        $data = curl_exec($ch);//运行curl
-        curl_close($ch);
-        $data = json_decode($data,1);//转化成数组输出
-        dd($data);
-        return message('Login successful','200', json_decode($data, true));
+        }
 
     }
 
