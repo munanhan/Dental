85a79e653dd7049ae3399aecc6bd164a4c30bbb6
munanhan <langxianwuxue@163.com>
2019-05-07 16:34:44 +0800

    han

--- server/app/Http/Controllers/Api/UserController.php
+++ server/app/Http/Controllers/Api/UserController.php
@@ -17,38 +17,38 @@ class UserController extends Controller
 
     public function index()
     {
-        return User::all();
+        return message('',User::all(),200);
     }
 
     public function getCurrentUser()
     {
-        return response()->json(Auth::user(),201);
+        return message('',Auth::user(),200);
     }
 
     public function show(User $user)
     {
-        return $user;
+        return message('',$user,200);
     }
 
     public function store(Request $request)
     {
         $user = User::create($request->all());
 
-        return response()->json($user, 201);
+        return message('',$user, 200);
     }
 
     public function update(Request $request, User $user)
     {
         $user->update($request->all());
 
-        return response()->json($user, 200);
+        return message('',$user, 200);
     }
 
     public function delete(User $user)
     {
         $user->delete();
 
-        return response()->json(null, 204);
+        return message('',null, 200);
     }
 
     public function getMessage()
