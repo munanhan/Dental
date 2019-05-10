4fc032ab6b0ec920fd11ccce41db19dcb46b3386
yuqi2060 <yuqi2060@163.com>
2019-05-10 19:51:46 +0800

    a

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,13 +3,15 @@ import * as home from './home'
 import * as user from './user'
 import * as appointment from './appointment'
 import * as operation_log from './operation_log'
+import * as patient_recycling_bin from './patient_recycling_bin'                                                   
 
 const apis = {
     base,
     home,
     user,
     appointment,
-    operation_log
+    operation_log,
+    patient_recycling_bin
 }
 
 const install = function (Vue) {
