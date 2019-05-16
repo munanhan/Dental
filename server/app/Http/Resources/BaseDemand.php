6549c118265bc5c9d0a8c0cb6e372d8540002a33
munanhan <langxianwuxue@163.com>
2019-05-16 18:39:00 +0800

    han

--- server/app/Http/Resources/BaseDemand.php
+++ server/app/Http/Resources/BaseDemand.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class User extends JsonResource
+class BaseDemand extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -17,8 +17,6 @@ class User extends JsonResource
         return [
             'id'=>$this->id,
             'name'=>$this->name,
-            'doctor'=>'',
-            'nurse'=>'',
         ];
     }
 }
---
+++ server/app/Model/BaseDemand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseDemand extends Model
+{
+    protected $guarded=['id,'];
+}
