13ba3452c83eff152cf7f70841d8bc087cf85896
yuqi2060 <yuqi2060@163.com>
2019-05-17 12:22:23 +0800

    merge

--- server/app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ server/app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+use Illuminate\Http\JsonResponse;
 
 use Closure;
 
@@ -18,6 +19,13 @@ class EnableCrossRequestMiddleware
         $response = $next($request);
         $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
         $allow_origin = config('config.allow_origin') != ''?config('config.allow_origin'):[];//获取允许跨域的列表
+        
+        if(mb_strtolower($request->getMethod())=='options')
+        {
+            $response = new JsonResponse($response, 204);
+            $response->header('Access-Control-Max-Age',3600);
+        }
+
         if (in_array($origin, $allow_origin)) {
             //设置跨域
             $response->header('Access-Control-Allow-Origin', $origin);
@@ -25,13 +33,9 @@ class EnableCrossRequestMiddleware
             $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
             $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS, DELETE');
             $response->header('Access-Control-Allow-Credentials', 'true');
+
         }
 
-        if(mb_strtolower($request->getMethod())=='options')
-        {
-            $response->header('Access-Control-Max-Age',3600);
-            return response()->json('',204);
-        }
 
         return $response;
     }
