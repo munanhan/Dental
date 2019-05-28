c23154fe0cb00ac73c73db4b23689785292feefd
munanhan <langxianwuxue@163.com>
2019-05-28 18:29:31 +0800

    han

--- client/src/views/patient/add_patient/AddPatient.vue
+++ client/src/views/patient/add_patient/AddPatient.vue
@@ -27,48 +27,14 @@
                         disable
                     ></el-input>
                 </el-form-item>
-                <!-- <i
-          class="el-icon-setting form-setting"
-         
-        ></i> -->
-                <!-- <i
-          class="fa fa-cog"
-          style="margin-top:10px;
-                 padding-left:10px;
-                 font-size:20px;
-                 cursor:pointer"
-        ></i> -->
-                <!-- 
-        <i
-                        class="fa fa-arrow-circle-left mr-10 pre-mth"
-                        @click.prevent.stop="switchDate('pre')"
-                    ></i> -->
 
-                <!-- <div
-                    class="block"
-                    style="margin-top:10px;
-                 margin-left:40px;
-                 margin-right:40px"
-                >
-                    <span class="demonstration"></span>
-                    <el-rate v-model="form.value1"></el-rate>
-                </div> -->
                 <div class="block">
                     <el-button
                         round
                         style="margin-left:50px"
                     >新增头像</el-button>
                 </div>
-                <!-- <i
-          class="el-icon-setting form-setting"
-         
-        ></i>
-        <i
-          class="el el-icon-question form-setting"
-          style="margin-top:10px;
-                 font-size:20px;
-                 cursor:pointer"
-        ></i> -->
+
             </div>
 
             <div style="display:flex">
@@ -91,22 +57,7 @@
                 </div>
 
             </div>
-            <!-- <div style="display:flex">
-        <el-form-item
-          label="医保余额"
-          prop="society_no"
-          style="width:300px"
-        >
-          <el-input v-model="form.society_no"></el-input>
-        </el-form-item>
-        <el-form-item
-          style="margin-left:30px;width:300px"
-          label="医保卡号"
-          prop="age"
-        >
-          <el-input v-model="form.age"></el-input>
-        </el-form-item>
-      </div> -->
+
             <div style="height:300px;overflow: auto">
                 <div style="display:flex">
                     <el-form-item
@@ -116,10 +67,7 @@
                     >
                         <el-input v-model="form.phone"></el-input>
                     </el-form-item>
-                    <!-- <i
-          class="el-icon-setting form-setting"
-         
-        ></i> -->
+
                     <el-form-item
                         style="margin-left:50px;width:300px"
                         label="电子邮箱"
@@ -154,50 +102,6 @@
                 </div>
                 <div style="display:flex">
                     <el-form-item
-                        style="width:300px"
-                        label="主治医生"
-                        prop="attendingphysician"
-                    >
-                        <el-select
-                            v-model="form.attendingphysician"
-                            style="width:220px"
-                            placeholder="请选择"
-                        >
-                            <el-option
-                                v-for="item in attend"
-                                :key="item.value"
-                                :label="item.name"
-                                :value="item.name"
-                            >
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item
-                        label="患者来源"
-                        prop="source"
-                        style="margin-left:50px;"
-                    >
-                        <el-select
-                            v-model="form.source"
-                            style="width:220px;"
-                        >
-                            <el-option
-                                v-for="item in sourceList"
-                                :key="item.id"
-                                :label="item.name"
-                                :value="item.name"
-                            >
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <i
-                        class="el-icon-setting form-setting"
-                        @click="Patient_source"
-                    ></i>
-
-                </div>
-                <div style="display:flex">
-                    <el-form-item
                         label="会员等级"
                         prop="member_level"
                     >
@@ -227,34 +131,6 @@
                     >
                         <el-input v-model="form.mem_card"></el-input>
                     </el-form-item>
-                </div>
-                
-                <div style="display:flex">
-                    <el-form-item
-                        label="就诊日期"
-                        prop="clinic_date"
-                    >
-                        <el-date-picker
-                            v-model="form.clinic_date"
-                            type="date"
-                            placeholder="选择日期"
-                        >
-                        </el-date-picker>
-                    </el-form-item>
-
-                    <div style="margin-top:10px;margin-left:60px">
-                        类&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                        别&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <el-radio
-                            v-model="form.category"
-                            label="1"
-                        >初诊</el-radio>
-                        <el-radio
-                            v-model="form.category"
-                            label="2"
-                        >复诊</el-radio>
-                    </div>
-
                 </div>
 
                 <div style="display:flex">
@@ -289,6 +165,44 @@
                     </el-form-item>
 
                 </div>
+                <div style="display:flex">
+                    <el-form-item
+                            label="民族"
+                            prop="nation"
+                            style="width:300px"
+                    >
+                        <el-input v-model="form.nation"></el-input>
+                    </el-form-item>
+
+                    <i
+                            class="el-icon-setting form-setting"
+                            @click="teeth_habit"
+                    ></i>
+                    <el-form-item
+                            label="洁牙习惯"
+                            prop="teeth_cleaning_habits"
+                            style="margin-left:20px;width:300px"
+                    >
+                        <el-select
+                                v-model="form.teeth_cleaning_habits"
+                                style="width:220px"
+                                multiple
+                                placeholder="请选择"
+                        >
+                            <el-option
+                                    v-for="item in teethList"
+                                    :key="item.value"
+                                    :label="item.name"
+                                    :value="item.name"
+                            >
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+
+                </div>
+                <div style="display:flex">
+
+                </div>
 
                 <div>
                     <el-form-item
@@ -309,12 +223,10 @@
                             >
                             </el-option>
                         </el-select>
-                        <!-- <i
-              class="el-icon-setting form-setting"
-              @click="contact_add"
-            ></i> -->
+
                     </el-form-item>
                 </div>
+
                 <div>
                     <el-form-item
                         label="过 敏 史"
@@ -385,152 +297,13 @@
                         ></i>
                     </el-form-item>
                 </div>
-                <div style="display:flex">
-                    <el-form-item
-                        label="社 保 号"
-                        prop="society_no"
-                    >
-                        <el-input
-                            style="width:220px"
-                            v-model="form.society_no"
-                        ></el-input>
 
-                    </el-form-item>
-                    <el-form-item
-                        style="margin-left:50px;width:300px"
-                        label="电网咨询"
-                        prop="grid_consulting"
-                    >
-                        <el-input v-model="form.grid_consulting"></el-input>
-                    </el-form-item>
 
-                    <!-- <el-form-item
-                        style="margin-left:50px;width:300px"
-                        label="电子邮箱"
-                        porp="email"
-                    >
-                        <el-input v-model="form.email"></el-input>
-                    </el-form-item> -->
-                </div>
-                <div style="display:flex">
-                    <el-form-item
-                        label="患者印象"
-                        prop="impression_id"
-                    >
-                        <el-select
-                            v-model="form.impression_id"
-                            style="width:220px"
-                            multiple
-                            filterable
-                            allow-create
-                            default-first-option
-                            placeholder="请选择"
-                        >
-                            <el-option
-                                v-for="item in impressionsList"
-                                :key="item.value"
-                                :label="item.name"
-                                :value="item.name"
-                            >
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <i
-                        class="el-icon-setting form-setting"
-                        @click="pat_impre"
-                    ></i>
-                    <el-form-item
-                        label="洁牙习惯"
-                        prop="teeth_cleaning_habits"
-                        style="margin-left:20px;width:300px"
-                    >
-                        <el-select
-                            v-model="form.teeth_cleaning_habits"
-                            style="width:220px"
-                            multiple
-                            placeholder="请选择"
-                        >
-                            <el-option
-                                v-for="item in teethList"
-                                :key="item.value"
-                                :label="item.name"
-                                :value="item.name"
-                            >
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
 
-                </div>
-                
-                <div style="display:flex">
-                    <!-- <el-form-item
-            label="民族"
-            porp="national"
-          >
-            <el-select
-              style="width:220px"
-              v-model="form.national"
-              placeholder="请选择"
-            >
-              <el-option
-                v-for="item in form.options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              >
-              </el-option>
-            </el-select>
-          </el-form-item> -->
-                    <el-form-item
-                        label="刷牙"
-                        prop="brush_teeth"
-                    >
-                        <div style="display:flex">
-                            <el-input
-                                style="width:60px"
-                                v-model="form.brush_day"
-                            ></el-input>
-                            <div style="margin-left:10px">次/天</div>
-                        </div>
-                    </el-form-item>
 
-                    <div style="display:flex">
-                        <el-input
-                            v-model="form.brush_minutes"
-                            style="width:60px;
-                             margin-left:10px"
-                        ></el-input>
-                        <div style="margin-left:10px;
-                        margin-top:13px">分钟/次</div>
-                    </div>
-                    <el-form-item
-                        label="吸烟"
-                        style="margin-left:38px"
-                        prop="smoking"
-                    >
-                        <div style="display:flex">
-                            <el-input
-                                style="width:100px"
-                                v-model="form.smoking"
-                            ></el-input>
-                            <div style="margin-left:10px">次/天</div>
-                        </div>
-                    </el-form-item>
-                </div>
-                <div style="display:flex">
-                    <el-form-item
-                        label="民族"
-                        prop="nation"
-                        style="width:300px"
-                    >
-                        <el-input v-model="form.nation"></el-input>
-                    </el-form-item>
 
-                    <i
-                        class="el-icon-setting form-setting"
-                        @click="teeth_habit"
-                    ></i>
-                </div>
+
+
                 <div style="display:flex">
 
                 </div>
@@ -541,7 +314,7 @@
             slot="footer"
             class="dialog-footer"
         >
-            <el-button>读身份证</el-button>
+
             <el-button>清空</el-button>
             <el-button
                 :loading="commitLoading"
