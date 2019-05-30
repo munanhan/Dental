661c71a21e9e4262f87510fb88753a7eaa29d359
munanhan <langxianwuxue@163.com>
2019-05-30 16:27:03 +0800

    han

--- server/app/Http/Controllers/Api/PatientMemberController.php
+++ server/app/Http/Controllers/Api/PatientMemberController.php
@@ -10,14 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class PatientMemberController extends BaseController
 {
-<<<<<<< HEAD
+
     public function store(Request $request)
     {
         PatientMember::updateOrInsert(['id'=>request('id')],$request->all());
         return message('','','200');
     }
-=======
 
-
->>>>>>> 4b235ec1e13e7c7224203564a1b76dc833a88133
 }
