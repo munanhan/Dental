1ca5c56c2ec255f64459c1c93f4ddea19e6c498d
munanhan <langxianwuxue@163.com>
2019-06-20 09:50:23 +0800

    han

--- server/app/Http/Controllers/Api/PatientCaseController.php
+++ server/app/Http/Controllers/Api/PatientCaseController.php
@@ -11,26 +11,7 @@ class PatientCaseController extends BaseController
 
     public function store(Request $request)
     {
-        //$data=$request->all();
-        $data=[
-            'status'=>'0',
-            'doctor'=>'chan',
-            'nurse'=>'zhu',
-            'treatment_date'=>'2019-5-21',
-            'main_complain'=>'主诉',
-            'now_history'=>'11',
-            'previous_history'=>'22',
-            'allergy_history'=>'33',
-            'doctor_advice'=>'44',
-            'case_info'=>[
-                ['type'=>1,'left_top'=>'12','left_bottom'=>'3','right_top'=>'4','right_bottom'=>'5','description'=>'12'],
-                ['type'=>1,'left_top'=>'12','left_bottom'=>'3','right_top'=>'4','right_bottom'=>'5','description'=>'dd1212'],
-                ['type'=>2,'left_top'=>'d','left_bottom'=>'a','right_top'=>'b','right_bottom'=>'c','description'=>'23'],
-                ['type'=>3,'left_top'=>'2a','left_bottom'=>'2d','right_top'=>'1','right_bottom'=>'2','description'=>'34'],
-                ['type'=>4,'left_top'=>'','left_bottom'=>'','right_top'=>'','right_bottom'=>'','description'=>'45'],
-                ['type'=>5,'left_top'=>'','left_bottom'=>'','right_top'=>'','right_bottom'=>'','description'=>'56'],
-            ]
-        ];
+        $data=$request->all();
 
         $patientCase=$this->createPatientCase($data);
 
